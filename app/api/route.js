533a872a77fcd6bcd0021a01a8562a0d5e6927d9
Hrithik Roshan <hrithikroshan1811@gmail.com>
2025-09-13 03:44:52 +0530

    fix: correct import path for inngest client

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
+import { inngest } from "../../inngest/client";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
 
 // Create an API that serves zero functions
